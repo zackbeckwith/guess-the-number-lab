14fc62c27e94d4117d897f01e14728a824be064f
zackbeckwith <z.beckwith99@gmail.com>
2022-01-30 22:21:21 -0500

    complete functional guess-the-number game

--- app.js
+++ app.js
@@ -2,9 +2,37 @@ const game = {
   title: 'Guess the Number!',
   biggestNum: 100,
   smallestNum: 1,
+  guess: null,
+  prevGuesses: [],
   secretNum: null,
+  getGuess: function () {
+    this.guess = parseInt(prompt(`Enter a guess between ${this.smallestNum} and ${this.biggestNum}`));
+
+    while (isNaN(this.guess) || this.guess >= this.biggestNum || this.guess <= this.smallestNum){
+      this.guess = parseInt(prompt(`Invalid answer. Enter a guess between ${this.smallestNum} and ${this.biggestNum}.`))
+      }
+    },
+
   play: function() {
     this.secretNum = Math.floor(Math.random() * 
-      (this.biggestNum - this.smallestNum + 1)) + this.smallestNum
+      (this.biggestNum - this.smallestNum + 1)) + this.smallestNum,
+
+    console.log(this.secretNum)
+    game.getGuess()
+
+    console.log(this.secretNum)
+    do {
+      this.prevGuesses.push(this.guess) 
+    
+      alert(`Incorrect answer. Try again.
+      Previous Attempts: ${this.prevGuesses}`)
+
+      console.log(this.prevGuesses)
+      ame.getGuess()
+      } while (this.guess !== this.secretNum);
+      alert('Correct!')
+
   }
-}``
+}
+
+game.play()
